b24b6c0821873969c40eb8dc0fde4ee10db09306
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-17 20:15:26 +0530

    completed navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import Landing from "./components/landlingPage/Landing";
+import Navbar from "./components/Navbar";
 import Signin from "./components/sign-in/Signin";
 import Signup from "./components/sign-up/Signup";
+import SignupTr from "./components/sign-up/SignupTr";
 // import { IoLockClosed } from "react-icons/io";
 function App() {
   return (
     <>
-      <Signup />
+      <Navbar />
+      <Signin />
     </>
   );
 }
---
+++ src/components/landlingPage/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="">
+        <h1>Welcome to Classroom</h1>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
