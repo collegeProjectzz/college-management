76c013dd8e0c10485a0ef41cb1ddbfc18245b4f7
siddhesh <iamsiddhesh22@gmail.com>
2022-01-08 17:05:13 +0530

    refactored navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,6 @@ import { useState } from "react/cjs/react.development";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
-  // { name: "Team", href: "#", current: false },
-  // { name: "Projects", href: "#", current: false },
-  // { name: "Calendar", href: "#", current: false }, 
 ];
 
 function classNames(...classes) {
