3072be026e1ffc10fd747f4d0d9f661027bed308
siddhesh <iamsiddhesh22@gmail.com>
2022-01-12 16:58:27 +0530

    update faculty profile

--- src/Dashboard/Faculty/Profile/Profile.jsx
+++ src/Dashboard/Faculty/Profile/Profile.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import useForm from '../../../hooks/useForm';
 
 export default function Profile() {
     const data = sessionStorage.getItem("faculty");
     const facultyData = data && JSON.parse(data);
+
+    const {
+        fId,
+        fName,
+        fEmail,
+        fPassword,
+        dNo
+    } = facultyData;
+
+    const { formData, handleInputChange } = useForm({
+        facId: fId,
+        facName: fName,
+        facEmail: fEmail,
+        facPassword: fPassword,
+        facDno: dNo
+    });
+
+    const {
+        facId,
+        facName,
+        facEmail,
+        facPassword,
+        facDno
+    } = formData;
+
+    const handleSubmit = () => {
+        fetch('http://localhost/college-backend/api/faculty/editFaculty.php', {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fId: facId,
+                fName: facName,
+                fEmail: facEmail,
+                fPassword: facPassword,
+                dNo: facDno
+            })
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    };
+
 
     return (
         <div className="flex flex-col p-2 w-full m-2 items-center justify-center md:flex-row">
@@ -13,10 +59,10 @@ export default function Profile() {
                     alt=""
                 />
                 <span class="bg-clip-text text-2xl font-semibold text-transparent bg-gradient-to-r from-cyan-600 to-blue-600">
-                    {facultyData.fName}
+                    {facName}
                 </span>
                 <span class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-600 to-blue-600">
-                    {facultyData.fId}
+                    {facId}
                 </span>
 
             </div>
@@ -26,13 +72,14 @@ export default function Profile() {
                 </span>
                 <div className="flex flex-col w-full py-0 p-16">
                     <div className="m-6 flex flex-col ">
-                        <label htmlFor="name" className="mr-2">
+                        <label htmlFor="facName" className="mr-2">
                             name:
                         </label>
                         <input
                             type="name"
-                            name="name"
-                            value={facultyData.fName}
+                            name="facName"
+                            value={facName}
+                            onChange={handleInputChange}
                             required
                             className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="name"
@@ -44,8 +91,8 @@ export default function Profile() {
                         </label>
                         <input
                             type="number"
-                            name="fId"
-                            value={facultyData.fId}
+                            name="facId"
+                            value={facId}
                             required
                             disabled
                             className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -58,43 +105,77 @@ export default function Profile() {
                         </label>
                         <input
                             type="email"
-                            name="email"
-                            value={facultyData.fEmail}
+                            name="facEmail"
+                            value={facEmail}
+                            onChange={handleInputChange}
+
                             required
                             className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="email"
                         />
                     </div>
                     <div className="m-6 flex flex-col ">
-                        <label htmlFor="password" className="mr-2">
+                        <label htmlFor="facPassword" className="mr-2">
                             password:
                         </label>
                         <input
                             type="password"
-                            name="password"
-                            value={facultyData.fPassword}
+                            name="facPassword"
+                            value={facPassword}
+                            onChange={handleInputChange}
                             required
                             className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="password"
                         />
                     </div>
                     <div className="m-6 flex flex-col ">
-                        <label htmlFor="password" className="mr-2">
+                        <label htmlFor="facDno" className="mr-2">
                             department no:
                         </label>
                         <input
                             type="text"
                             disabled
-                            name="sem"
-                            value={facultyData.dNo}
+                            name="facDno"
+                            value={facDno}
                             required
                             className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                             placeholder="Enter password"
                         />
                     </div>
-
+                    <div className="flex justify-center items-center">
+                        <button
+                            onClick={handleSubmit}
+                            className="flex justify-center items-center m-5 rounded-full p-3 bg-gradient-to-r from-blue-600 to-sky-300 hover:from-blue-600 hover:to-sky-200"
+                        >
+                            <span className="flex items-center justify-center text-white">
+                                Update <FaSignInAlt className="ml-2" />
+                            </span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
+
+// {!state ? (
+//     <span className="flex items-center justify-center text-white">
+//         Update <FaSignInAlt className="ml-2" />
+//     </span>
+// ) : (
+//     <svg
+//         class="w-6 h-6 mx-auto animate-spin"
+//         xmlns="http://www.w3.org/2000/svg"
+//         fill="none"
+//         viewBox="0 0 24 24"
+//         stroke="currentColor"
+//     >
+//         <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             stroke-width="2"
+//             d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+//         />
+//     </svg>
+// )}
